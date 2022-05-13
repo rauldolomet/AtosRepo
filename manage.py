4538cc167388208a61f5e1c58e23effd26bb36a7
Dolomet <raul-dan.dolomet@atos.net>
2022-05-13 15:48:54 +0300

    New folder structure

--- manage.py
+++ manage.py
@@ -1,1 +1,22 @@
-print("helo")
+from crypt import methods
+from operator import methodcaller
+from flask import Flask
+from flask import request
+app=Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "hello world"
+
+# @app.route("/greet")
+# def greet():
+#     return "Hello!"
+
+# @app.route("/greet/<name>")
+# def greetName(name):
+#     return "Hello, " + name
+
+# @app.route("/bye", methods=['GET'])
+# def Bye():
+#     print(request.form)
+#     return "Draga " + request.form["for"] + ", iti urez " + request.form["wish"]
