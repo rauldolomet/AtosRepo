25afa5c256cdd0343c5750a2330e1dbebafdd4bc
Dolomet <raul-dan.dolomet@atos.net>
2022-05-16 10:10:41 +0300

    Update Luni

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 
 from .config import Config
+from .home import home as home_blueprint
 
 app=Flask(__name__)
 app.secret_key = "aksnd1#kj$%"
 app.config.from_object(Config)
 
 def create_app():
+    app.register_blueprint(home_blueprint)
     return app
